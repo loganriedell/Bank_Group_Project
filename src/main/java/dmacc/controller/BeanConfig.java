87ae3b4f8806a00d42b54bed064213b98515b2c8
loganriedell <66161459+loganriedell@users.noreply.github.com>
2021-03-31 18:17:27 -0500

    Initial commit

---
+++ src/main/java/dmacc/controller/BeanConfig.java
@@ -0,0 +1,9 @@
+package dmacc.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig {
+	
+}
