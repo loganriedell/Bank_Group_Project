2fb298097aa47d5945bab8402ef7f04dab738b84
Ethan Dorenkamp <58747696+EthanRd6@users.noreply.github.com>
2021-04-12 00:23:11 -0500

    Create Employee class

---
+++ src/main/java/dmacc/beans/Employee.java
@@ -0,0 +1,95 @@
+package dmacc.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @author Ethan Dorenkamp - edorenkamp
+ * CIS175 - Spring 2021
+ * Apr 6, 2021
+ */
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int employeeId;
+	private String firstName;
+	private String lastName;
+	private float payRate;
+	private String email;
+	
+	public Employee() {
+		super();
+	}
+
+	public Employee(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public Employee(String firstName, String lastName, float payRate, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.payRate = payRate;
+		this.email = email;
+	}
+
+	public Employee(int employeeId, String firstName, String lastName, float payRate, String email) {
+		super();
+		this.employeeId = employeeId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.payRate = payRate;
+		this.email = email;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public float getPayRate() {
+		return payRate;
+	}
+
+	public void setPayRate(float payRate) {
+		this.payRate = payRate;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", payRate=" + payRate + ", email=" + email + "]";
+	}
+}
