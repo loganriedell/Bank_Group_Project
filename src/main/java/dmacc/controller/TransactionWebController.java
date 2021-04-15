66947b8202d9bf277f742fd0553ce12837b81efb
loganriedell <66161459+loganriedell@users.noreply.github.com>
2021-04-15 00:35:59 -0500

    Ethans Web Controller add

--- src/main/java/dmacc/controller/TransactionWebController.java
+++ src/main/java/dmacc/controller/TransactionWebController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import dmacc.beans.Transaction;
 import dmacc.beans.Account;
@@ -24,16 +25,19 @@ public class TransactionWebController {
 	
 	@GetMapping("/newTransaction/{id}")
 	public String addTransaction(@PathVariable("id") long id, Model model) {
-		Account accnt = accntRepo.findById(id);
+		Account accnt = accntRepo.findById(id).orElse(null);
+		model.addAttribute("account", accnt);
 		Transaction t = new Transaction();
+		String action = "";
+		model.addAttribute("action", action);
 		model.addAttribute("newTransaction", t);
 		return "withdrawal/depostit";	
 		
 	}
 	@PostMapping("/newTransaction/{id}")
-	public String addTransaction(@PathVariable("id") long id, @ModelAttribute Transaction t, Model model) {
-		Account accnt = accntRepo.findById(id).orElse(null);
-		accnt.deposit(t.getAmount());
+	public String addTransaction(@PathVariable("id") long id, @ModelAttribute Transaction t, @ModelAttribute Account accnt, 
+			@RequestParam String action, Model model) {
+		if model.
 		return "";
 	}
 }
