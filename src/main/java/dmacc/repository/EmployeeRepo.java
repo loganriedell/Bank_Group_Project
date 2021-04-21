4664c1c9555eaa81e6bcf6fac53a7efddce5a8e5
jjwood1 <61518642+jjwood1@users.noreply.github.com>
2021-04-20 20:35:56 -0500

    login-added

--- src/main/java/dmacc/repository/EmployeeRepo.java
+++ src/main/java/dmacc/repository/EmployeeRepo.java
@@ -1,8 +1,11 @@
 package dmacc.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dmacc.beans.Employee;
+import dmacc.beans.User;
 
 /**
  * @author Ethan Dorenkamp - edorenkamp
@@ -10,5 +13,5 @@ import dmacc.beans.Employee;
  * Apr 14, 2021
  */
 public interface EmployeeRepo extends JpaRepository<Employee, Long>{
-
+	List<Employee>findByEmail(String email);
 }
